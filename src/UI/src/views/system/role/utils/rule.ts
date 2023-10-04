57c06805507eb6d4db56e4309eb8c304be6f9a9f
凌少 <Miclyp@outlook.com>
2023-10-04 14:21:17 +0800

    删除旧UI

--- src/UI/src/views/system/role/utils/rule.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from "vue";
-import type { FormRules } from "element-plus";
-
-/** 自定义表单规则校验 */
-export const formRules = reactive(<FormRules>{
-  roleName: [{ required: true, message: "角色名称为必填项", trigger: "blur" }],
-  roleCode: [{ required: true, message: "角色标识为必填项", trigger: "blur" }],
-  description: [{ required: true, message: "描述为必填项", trigger: "blur" }]
-});
--- src/UI/src/views/system/testtable/utils/rule.ts
+++
@@ -1,9 +0,0 @@
-﻿import { reactive } from "vue";
-import type { FormRules } from "element-plus";
-
-/** 自定义表单规则校验 */
-export const formRules = reactive(<FormRules>{
-  name: [{ required: true, message: "名称为必填项", trigger: "blur" }],
-
-  description: [{ required: true, message: "描述为必填项", trigger: "blur" }],
-});
