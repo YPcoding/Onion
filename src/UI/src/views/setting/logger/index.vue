02dd9bad926c6b06394eb564b775bd0923043968
凌少 <Miclyp@outlook.com>
2023-10-18 02:26:26 +0800

    修改前端日志时间显示字段

--- src/UI/src/views/setting/logger/index.vue
+++ src/UI/src/views/setting/logger/index.vue
@@ -1,7 +1,9 @@
 <template>
 	<el-container>
 		<el-aside width="220px">
-			<el-tree ref="category" @node-click="categoryClick" class="menu" node-key="label" :data="category" :default-expanded-keys="['系统日志']" current-node-key="系统日志" :highlight-current="true" :expand-on-click-node="false">
+			<el-tree ref="category" @node-click="categoryClick" class="menu" node-key="label" :data="category"
+				:default-expanded-keys="['系统日志']" current-node-key="系统日志" :highlight-current="true"
+				:expand-on-click-node="false">
 			</el-tree>
 		</el-aside>
 		<el-container>
@@ -9,7 +11,9 @@
 				<el-container>
 					<el-header>
 						<div class="left-panel">
-							<el-date-picker v-model="date"  @change="handlePick" type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" value-format="YYYY-MM-DD HH:mm:ss"></el-date-picker>
+							<el-date-picker v-model="date" @change="handlePick" type="datetimerange" range-separator="至"
+								start-placeholder="开始日期" end-placeholder="结束日期"
+								value-format="YYYY-MM-DD HH:mm:ss"></el-date-picker>
 						</div>
 						<div class="right-panel">
 
@@ -19,23 +23,30 @@
 						<scEcharts height="100%" :option="logsChartOption"></scEcharts>
 					</el-header>
 					<el-main class="nopadding">
-						<scTable ref="table" :data="dataList" :page-size="pageSize" @pagination-change="handlePaginationChange" @pagesize="pageSizeChange" stripe highlightCurrentRow @row-click="rowClick">
+						<scTable ref="table" :data="dataList" :page-size="pageSize"
+							@pagination-change="handlePaginationChange" @pagesize="pageSizeChange" stripe
+							highlightCurrentRow @row-click="rowClick">
 							<el-table-column label="级别" prop="level" width="50">
 								<template #default="scope">
-									<el-icon v-if="scope.row.level=='Fatal'" style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
-									<el-icon v-if="scope.row.level=='Critical'" style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
-									<el-icon v-if="scope.row.level=='Error'" style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
-									<el-icon v-if="scope.row.level=='Warning'" style="color: #E6A23C;"><el-icon-warning-filled /></el-icon>
-									<el-icon v-if="scope.row.level=='Information'" style="color: #409EFF;"><el-icon-info-filled /></el-icon>
+									<el-icon v-if="scope.row.level == 'Fatal'"
+										style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
+									<el-icon v-if="scope.row.level == 'Critical'"
+										style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
+									<el-icon v-if="scope.row.level == 'Error'"
+										style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
+									<el-icon v-if="scope.row.level == 'Warning'"
+										style="color: #E6A23C;"><el-icon-warning-filled /></el-icon>
+									<el-icon v-if="scope.row.level == 'Information'"
+										style="color: #409EFF;"><el-icon-info-filled /></el-icon>
 								</template>
 							</el-table-column>
 							<el-table-column label="ID" prop="ID" width="180"></el-table-column>
-							<el-table-column label="日志名" prop="LoggerName" width="150"></el-table-column>
+							<el-table-column label="日志名" prop="LoggerName" width="200"></el-table-column>
 							<el-table-column label="请求接口" prop="RequestPath" width="260"></el-table-column>
-							<el-table-column label="请求方法" prop="RequestMethod" width="80"></el-table-column>
-							<el-table-column label="用户" prop="UserName" width="50"></el-table-column>
+							<el-table-column label="请求方法" prop="RequestMethod" width="70"></el-table-column>
+							<el-table-column label="用户" prop="UserName" width="150"></el-table-column>
 							<el-table-column label="客户端IP" prop="ClientIP" width="100"></el-table-column>
-							<el-table-column label="日志时间" prop="TimeStamp" width="200"></el-table-column>
+							<el-table-column label="日志时间" prop="LoggerTime" width="150"></el-table-column>
 						</scTable>
 					</el-main>
 				</el-container>
@@ -49,159 +60,157 @@
 </template>
 
 <script>
-	import info from './info'
-	import scEcharts from '@/components/scEcharts'
+import info from './info'
+import scEcharts from '@/components/scEcharts'
 
-	export default {
-		name: 'logger',
-		components: {
-			info,
-			scEcharts
-		},
-		data() {
-			return {
-				currentPage: 1, // 当前页数
-                pageSize: 10, // 每页显示的条数
-				infoDrawer: false,
-				logsChartOption: {
-					color: ['#409eff','#e6a23c','#f56c6c'],
-					grid: {
-						top: '0px',
-						left: '10px',
-						right: '10px',
-						bottom: '0px'
-					},
-					tooltip: {
-						trigger: 'axis'
-					},
-					xAxis: {
-						type: 'category',
-						boundaryGap: false,
-						data: ['2021-07-01', '2021-07-02', '2021-07-03', '2021-07-04', '2021-07-05', '2021-07-06', '2021-07-07', '2021-07-08', '2021-07-09', '2021-07-10', '2021-07-11', '2021-07-12', '2021-07-13', '2021-07-14', '2021-07-15']
-					},
-					yAxis: {
-						show: false,
-						type: 'value'
-					},
-					series: [{
-						data: [120, 200, 150, 80, 70, 110, 130, 120, 200, 150, 80, 70, 110, 130, 70, 110],
-						type: 'bar',
-						stack: 'log',
-						barWidth: '15px'
-					},
-					{
-						data: [15, 26, 7, 12, 13, 9, 21, 15, 26, 7, 12, 13, 9, 21, 12, 3],
-						type: 'bar',
-						stack: 'log',
-						barWidth: '15px'
-					},
-					{
-						data: [0, 0, 0, 120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-						type: 'bar',
-						stack: 'log',
-						barWidth: '15px'
-					}]
+export default {
+	name: 'logger',
+	components: {
+		info,
+		scEcharts
+	},
+	data () {
+		return {
+			currentPage: 1, // 当前页数
+			pageSize: 10, // 每页显示的条数
+			infoDrawer: false,
+			logsChartOption: {
+				color: ['#409eff', '#e6a23c', '#f56c6c'],
+				grid: {
+					top: '0px',
+					left: '10px',
+					right: '10px',
+					bottom: '0px'
 				},
-				category: [
-					{
-						label: '系统日志',
-						children: [
-							{label: 'Information'},						
-							{label: 'Critical'},
-							{label: 'Warning'},
-							{label: 'Error'},
-							{label: 'Fatal'}
-						]
-					},
-					// {
-					// 	label: '应用日志',
-					// 	children: [
-					// 		{label: 'selfHelp'},
-					// 		{label: 'WechatApp'}
-					// 	]
-					// }
-				],
-				date: [],
-				dataList:[],
-				search: {
-					keyword: "",
-					level:"",
-					startDateTime: null,
-                    endDateTime: null
+				tooltip: {
+					trigger: 'axis'
+				},
+				xAxis: {
+					type: 'category',
+					boundaryGap: false,
+					data: ['2021-07-01', '2021-07-02', '2021-07-03', '2021-07-04', '2021-07-05', '2021-07-06', '2021-07-07', '2021-07-08', '2021-07-09', '2021-07-10', '2021-07-11', '2021-07-12', '2021-07-13', '2021-07-14', '2021-07-15']
+				},
+				yAxis: {
+					show: false,
+					type: 'value'
+				},
+				series: [{
+					data: [120, 200, 150, 80, 70, 110, 130, 120, 200, 150, 80, 70, 110, 130, 70, 110],
+					type: 'bar',
+					stack: 'log',
+					barWidth: '15px'
+				},
+				{
+					data: [15, 26, 7, 12, 13, 9, 21, 15, 26, 7, 12, 13, 9, 21, 12, 3],
+					type: 'bar',
+					stack: 'log',
+					barWidth: '15px'
+				},
+				{
+					data: [0, 0, 0, 120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					type: 'bar',
+					stack: 'log',
+					barWidth: '15px'
+				}]
+			},
+			category: [
+				{
+					label: '系统日志',
+					children: [
+						{ label: 'Information' },
+						{ label: 'Critical' },
+						{ label: 'Warning' },
+						{ label: 'Error' },
+						{ label: 'Fatal' }
+					]
+				},
+				// {
+				// 	label: '应用日志',
+				// 	children: [
+				// 		{label: 'selfHelp'},
+				// 		{label: 'WechatApp'}
+				// 	]
+				// }
+			],
+			date: [],
+			dataList: [],
+			search: {
+				keyword: "",
+				level: "",
+				startDateTime: null,
+				endDateTime: null
+			}
+		}
+	},
+	async mounted () {
+		this.upsearch()
+	},
+	methods: {
+		async upsearch () {
+			let response = await this.$API.system.log.systemList.post({
+				pageNumber: this.currentPage,
+				pageSize: this.pageSize,
+				orderBy: "Id",
+				sortDirection: "Descending",
+				level: this.search.level,
+				startDateTime: this.search.startDateTime,
+				endDateTime: this.search.endDateTime
+				//keyword:this.search.keyword
+			})
+			const list = []
+			for (const item of response.data.items) {
+				let data = JSON.parse(item.properties)
+				data.level = item.level
+				data.exception = item.exception
+				list.push(data)
+			}
+			this.$refs.table.total = response.data.totalItems
+			this.dataList = list
+
+			response = await this.$API.system.log.countDailys.get()
+			if (response.succeeded) {
+				this.logsChartOption.xAxis.data = response.data.xAxis
+
+				// 获取今天的日期
+				var today = new Date().toISOString().slice(0, 10)
+				// 遍历日期数组
+				for (var i = 0; i < this.logsChartOption.xAxis.data.length; i++) {
+					if (this.logsChartOption.xAxis.data[i] === today) {
+						this.logsChartOption.xAxis.data[i] = "今天"
+					}
 				}
+
+				this.logsChartOption.series[0].data = response.data.informationConut
+				this.logsChartOption.series[1].data = response.data.warningConut
+				this.logsChartOption.series[2].data = response.data.errorConut
 			}
 		},
-		async mounted() {
-			this.upsearch()
-		},
-		methods: {
-		async	upsearch(){
-				let response =  await this.$API.system.log.systemList.post({
-					pageNumber:this.currentPage,
-					pageSize:this.pageSize,
-					orderBy: "Id",
-					sortDirection: "Descending",
-					level:this.search.level,
-					startDateTime :this.search.startDateTime,
-					endDateTime :this.search.endDateTime
-					//keyword:this.search.keyword
-				})
-				const list = [];
-				for (const item of response.data.items) 
-				{
-					let data = JSON.parse(item.properties);
-					data.level = item.level
-					data.exception = item.exception
-					list.push(data);
-				}
-				this.$refs.table.total = response.data.totalItems;
-				this.dataList=list
-	
-				response =  await this.$API.system.log.countDailys.get()
-				if (response.succeeded) {
-					this.logsChartOption.xAxis.data=response.data.xAxis;
-
-					// 获取今天的日期
-                   var today = new Date().toISOString().slice(0, 10);
-				   // 遍历日期数组
-                   for (var i = 0; i < this.logsChartOption.xAxis.data.length; i++) {
-                        if (this.logsChartOption.xAxis.data[i] === today) {
-                            this.logsChartOption.xAxis.data[i] = "今天";
-                        }
-                    }
-
-					this.logsChartOption.series[0].data=response.data.informationConut;
-					this.logsChartOption.series[1].data=response.data.warningConut;
-					this.logsChartOption.series[2].data=response.data.errorConut;
-				}
-		},
-		rowClick(row){
+		rowClick (row) {
 			this.infoDrawer = true
 			this.$nextTick(() => {
 				this.$refs.info.setData(row)
 			})
 		},
 		//点击分页
-		handlePaginationChange(val){		
-				this.currentPage = val;
-				this.upsearch();
+		handlePaginationChange (val) {
+			this.currentPage = val
+			this.upsearch()
 		},
-		    pageSizeChange(size){
-				this.pageSize = size;
-				this.upsearch();
-		    },//树点击事件
-			categoryClick(data){		
-				this.search.level = data.label
-				this.upsearch()
-			},
-			handlePick(val) {
-                this.search.startDateTime =val[0]+".000"
-				this.search.endDateTime =val[1]+".000"
-				this.upsearch()
-            }
+		pageSizeChange (size) {
+			this.pageSize = size
+			this.upsearch()
+		},//树点击事件
+		categoryClick (data) {
+			this.search.level = data.label
+			this.upsearch()
+		},
+		handlePick (val) {
+			this.search.startDateTime = val[0] + ".000"
+			this.search.endDateTime = val[1] + ".000"
+			this.upsearch()
 		}
 	}
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- src/UI/src/views/userCenter/user/logs.vue
+++ src/UI/src/views/userCenter/user/logs.vue
@@ -1,14 +1,18 @@
 <template>
 	<el-card shadow="never" header="近7天操作记录">
-		<scTable ref="table" :page-size="pageSize" @pagination-change="handlePaginationChange" @pagesize="pageSizeChange" height="auto" paginationLayout="total, prev, pager, next" hideDo>
+		<scTable ref="table" :page-size="pageSize" @pagination-change="handlePaginationChange" @pagesize="pageSizeChange"
+			height="auto" paginationLayout="total, prev, pager, next" hideDo>
 			<sc-table-column label="序号" type="index"></sc-table-column>
 			<el-table-column label="级别" prop="level" width="50">
-					<template #default="scope">
-						<el-icon v-if="scope.row.level=='Error'" style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
-						<el-icon v-if="scope.row.level=='Warning'" style="color: #E6A23C;"><el-icon-warning-filled /></el-icon>
-					<el-icon v-if="scope.row.level=='Information'" style="color: #409EFF;"><el-icon-info-filled /></el-icon>
+				<template #default="scope">
+					<el-icon v-if="scope.row.level == 'Error'"
+						style="color: #F56C6C;"><el-icon-circle-close-filled /></el-icon>
+					<el-icon v-if="scope.row.level == 'Warning'"
+						style="color: #E6A23C;"><el-icon-warning-filled /></el-icon>
+					<el-icon v-if="scope.row.level == 'Information'"
+						style="color: #409EFF;"><el-icon-info-filled /></el-icon>
 				</template>
-			</el-table-column>		
+			</el-table-column>
 			<sc-table-column label="业务名称" prop="LoggerName" min-width="200"></sc-table-column>
 			<sc-table-column label="IP" prop="ClientIP" width="150"></sc-table-column>
 			<sc-table-column label="响应码" prop="ResponseStatusCode" width="150">
@@ -21,48 +25,46 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				currentPage: 1, // 当前页数
-                pageSize: 10, // 每页显示的条数
-			}
-		},
-		async mounted() {
-			this.query();
-		},
-		methods: {
-		    async query(){
-				let response =  await await this.$API.system.log.list.post({
-					pageNumber:this.currentPage,
-					pageSize:this.pageSize,
-					orderBy: "Id",
-					sortDirection: "Descending",
-					//keyword:this.search.keyword
-				})
+export default {
+	data () {
+		return {
+			currentPage: 1, // 当前页数
+			pageSize: 10, // 每页显示的条数
+		}
+	},
+	async mounted () {
+		this.query()
+	},
+	methods: {
+		async query () {
+			let response = await await this.$API.system.log.list.post({
+				pageNumber: this.currentPage,
+				pageSize: this.pageSize,
+				orderBy: "Id",
+				sortDirection: "Descending",
+				//keyword:this.search.keyword
+			})
 
-				const list = [];
-				for (const item of response.data.items) 
-				{
-					let data = JSON.parse(item.properties);
-					data.level = item.level
-					list.push(data);
-				}
-			    
-				this.$refs.table.total = response.data.totalItems;
-				this.$refs.table.tableData = list;
-		    },//点击分页
-			handlePaginationChange(val){		
-				this.currentPage = val;
-				this.query();
-			},
-			pageSizeChange(size){
-				this.pageSize = size;
-				this.query();
+			const list = []
+			for (const item of response.data.items) {
+				let data = JSON.parse(item.properties)
+				data.level = item.level
+				list.push(data)
 			}
-	    },
-	}
+
+			this.$refs.table.total = response.data.totalItems
+			this.$refs.table.tableData = list
+		},//点击分页
+		handlePaginationChange (val) {
+			this.currentPage = val
+			this.query()
+		},
+		pageSizeChange (size) {
+			this.pageSize = size
+			this.query()
+		}
+	},
+}
 </script>
 
-<style>
-</style>
+<style></style>
